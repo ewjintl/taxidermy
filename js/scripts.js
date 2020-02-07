908439d815bb3875fa73a9daf330f3ce1602d67d
Dasha Maletis and elliot jordan <dasha@>
2020-02-06 20:54:24 -0800

    s

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	$("p").click(function() {
+		$("p").addClass("clickable");
+	});
+
 	$("button#dark-mode").click(function() {
 		// $("body").removeClass();
 		$("body").toggleClass("dark-mode");   
